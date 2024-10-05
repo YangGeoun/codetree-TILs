270131bae3c3919d624fec6cc4dc6de6f85c4ff4
YangGeonu <113342888+YangGeoun@users.noreply.github.com>
2024-10-05 16:36:43 +0900

    Title: 특정 문자열 존재유무, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

--- 241005/특정 문자열 존재유무/specific-string-present-or-not.java
+++ 241005/특정 문자열 존재유무/specific-string-present-or-not.java
@@ -1,21 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        String str = st.nextToken();
-
-        for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            if (line.contains(str)){
-                System.out.println(line);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        String s = st.nextToken();
+        for(int i=0;i<N;i++){
+            String input = in.readLine();
+            if(input.contains(s)){
+                System.out.println(input);
             }
         }
+        
     }
 }
