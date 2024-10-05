cc86b341190781b63de7ce98f3c951029c40580a
YangGeonu <113342888+YangGeoun@users.noreply.github.com>
2024-10-05 19:59:30 +0900

    Title: 문자 삼각형 출력하기 1, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241005/문자 삼각형 출력하기 1/print-char-triangle-1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IoException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in);
+        System.out.print(n);
+    }
+    
+
+}
