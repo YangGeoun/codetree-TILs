fcef5653c0d1b03ef2d4fccdc3a8f045f64896e8
YangGeonu <113342888+YangGeoun@users.noreply.github.com>
2024-10-06 22:41:50 +0900

    Title: 문자 삼각형 출력하기 1, Time: 190ms, Memory: 11MB, Status: Passed - CodeTree

--- 241006/문자 삼각형 출력하기 1/print-char-triangle-1.java
+++ 241006/문자 삼각형 출력하기 1/print-char-triangle-1.java
@@ -6,14 +6,16 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(in.readLine());
+        int start = 65;
         for (int i = 0 ; i < n ; i++) {
-            int tmp = 65 + i;
+            int tmp = start + (i % 26);
             for (int j = 0; j < 2 * (n - i - 1) ; j++) {
                 System.out.print(" ");
             }
             System.out.print((char)tmp);
             for (int j = -1; j < i - 1 ; j++) {
                 tmp = (tmp + ((n - j - 2) % 26));
+                if (tmp > 90) tmp = tmp - 26; 
                 System.out.print(" "+ (char)tmp);
             }
             System.out.println();
