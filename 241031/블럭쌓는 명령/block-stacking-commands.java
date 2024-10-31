0856321d4677ec1b20af52a5393848a4daa114ff
YangGeonu <113342888+YangGeoun@users.noreply.github.com>
2024-10-31 11:15:47 +0900

    Title: 블럭쌓는 명령, Time: 264ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/블럭쌓는 명령/block-stacking-commands.java
+++ 241031/블럭쌓는 명령/block-stacking-commands.java
@@ -3,34 +3,37 @@ import java.util.*;
 
 public class Main {
     static int N, K;
-    static int[] blockList;
-    static int[] count;
-    // 
+    static int[] diff;
+
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line;
-        line = br.readLine().split(" ");
+        String[] line = br.readLine().split(" ");
         N = Integer.parseInt(line[0]);
-        blockList = new int[N];
         K = Integer.parseInt(line[1]);
-        count = new int[K + 1];
+        diff = new int[N + 1];
+
         for (int i = 0; i < K; i++) {
             line = br.readLine().split(" ");
-            for (int j = Integer.parseInt(line[0]) - 1; j < Integer.parseInt(line[1]); j++) {
-                blockList[j]++;
-            } 
+            int start = Integer.parseInt(line[0]) - 1;
+            int end = Integer.parseInt(line[1]);
+            diff[start]++;
+            diff[end]--;
         }
-        for(int el : blockList) {
-            count[el]++;
-        }
-        int index = 0;
-        int cnt = 0;
-        while(cnt < (N / 2 + 1)) {
-            cnt += count[index++];
-            
-        }
-        System.out.println(index - 1);
 
+        int sum = 0;
+        int[] count = new int[K + 1];
+        for (int i = 0; i < N; i++) {
+            sum += diff[i];
+            count[sum]++;
+        }
+
+        int total = 0;
+        for (int i = K; i >= 0; i--) {
+            total += count[i];
+            if (total >= (N + 1) / 2) {
+                System.out.println(i);
+                break;
+            }
+        }
     }
 }
