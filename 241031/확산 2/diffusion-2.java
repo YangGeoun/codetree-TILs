400c813e0ece652d25c50107273793707d50a892
YangGeonu <113342888+YangGeoun@users.noreply.github.com>
2024-10-31 14:04:47 +0900

    Title: 확산 2, Time: 1228ms, Memory: 82MB, Status: Passed - CodeTree

--- 241031/확산 2/diffusion-2.java
+++ 241031/확산 2/diffusion-2.java
@@ -3,57 +3,56 @@ import java.util.*;
 
 public class Main {
     static int[][] matrix;
+    static boolean[][] visited;
     static int[] dr = {0, 0, -1, 1};
-    static int[] dc = {-1, 1 ,0, 0};
-    static class Point {
-        int r, c;
-        Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
+    static int[] dc = {-1, 1, 0, 0};
     static int numOfZero = 0;
     static int n, m;
-    static Queue<Point> q = new LinkedList<>();
+
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        String[] line;
+        line = br.readLine().split(" ");
+        n = Integer.parseInt(line[0]);
+        m = Integer.parseInt(line[1]);
         matrix = new int[n][m];
+        visited = new boolean[n][m];
+        Queue<int[]> q = new ArrayDeque<>();
+
         for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+            line = br.readLine().split(" ");
             for(int j = 0; j < m; j++){
-                int num = Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(line[j]);
                 if(num == 1) {
-                    q.add(new Point(i, j));
+                    q.offer(new int[]{i, j, 0});
+                    visited[i][j] = true;
                 } else if (num == 0) numOfZero++;
                 matrix[i][j] = num;
             }
         }
-        System.out.println(bfs());
+        System.out.println(bfs(q));
     }
-    static int bfs() {
+
+    static int bfs(Queue<int[]> q) {
         if (numOfZero == 0) return 0;
-        int answer = 0;
+        int maxDays = 0;
         int cnt = 0;
+
         while(!q.isEmpty()){
-            Point now = q.poll();
+            int[] now = q.poll();
+            int r = now[0], c = now[1], days = now[2];
+
             for (int d = 0; d < 4; d++) {
-                int nr = now.r + dr[d];
-                int nc = now.c + dc[d];
-                if (0 <= nr && 0 <= nc && nr < n && nc < m){
-                    if (matrix[nr][nc] == 0) {
-                        matrix[nr][nc] = matrix[now.r][now.c] + 1;
-                        answer = matrix[nr][nc];
-                        cnt++;
-                        q.add(new Point(nr,nc));
-                    }
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+                if (0 <= nr && nr < n && 0 <= nc && nc < m && !visited[nr][nc] && matrix[nr][nc] == 0){
+                    visited[nr][nc] = true;
+                    q.offer(new int[]{nr, nc, days + 1});
+                    maxDays = Math.max(maxDays, days + 1);
+                    if (++cnt == numOfZero) return maxDays;
                 }
             }
         }
-        if (cnt == numOfZero) return answer - 1;
         return -1;
     }
 }
